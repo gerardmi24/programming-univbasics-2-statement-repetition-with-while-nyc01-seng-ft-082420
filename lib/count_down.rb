0434a68dea991fbd78ec64e96db55f17c46ea7b4
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-24 02:26:08 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
-hny_num = 10
+newyear_num = 10
 
-while hny_num >= 1 do
+while newyear_num >= 1 do
   puts #{hny_num}
-  hny_num -= 1
+  newyear_num -= 1
 end
   puts "Happy New Year!"
 end
