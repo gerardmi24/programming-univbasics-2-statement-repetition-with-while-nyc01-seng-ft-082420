ac48baee7c67b5d2f0f9ddf9518710594ecbd990
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-24 02:28:48 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
 newyear_num = 10
 exit_num = 0
-while newyear_num >= 1 do
+while newyear_num > exit_num do
   break if newyear_num == exit_num
   puts "#{newyear_num}"
   newyear_num -= 1
